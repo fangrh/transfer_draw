8c27e34299bd994a027f060c2c6898656a43a275
fangrh <ruihuanfang@gmail.com>
2024-06-28 16:08:31 +0300

    Almost Done, but the miror cannot apply in CV

--- control_ui.py
+++ control_ui.py
@@ -15,18 +15,18 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QGridLayout, QLabel,
-    QLineEdit, QPushButton, QSizePolicy, QSlider,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDoubleSpinBox,
+    QGridLayout, QLabel, QPushButton, QSizePolicy,
+    QSlider, QWidget)
 
 class Ui_Controller(object):
     def setupUi(self, Controller):
         if not Controller.objectName():
             Controller.setObjectName(u"Controller")
-        Controller.resize(313, 292)
+        Controller.resize(275, 383)
         self.widget = QWidget(Controller)
         self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 20, 297, 259))
+        self.widget.setGeometry(QRect(10, 25, 244, 311))
         self.gridLayout = QGridLayout(self.widget)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
@@ -38,17 +38,19 @@ class Ui_Controller(object):
         self.label_9 = QLabel(self.widget)
         self.label_9.setObjectName(u"label_9")
 
-        self.gridLayout.addWidget(self.label_9, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.label_9, 0, 1, 1, 2)
 
-        self.SizeSpeed = QLineEdit(self.widget)
+        self.SizeSpeed = QDoubleSpinBox(self.widget)
         self.SizeSpeed.setObjectName(u"SizeSpeed")
+        self.SizeSpeed.setValue(0.100000000000000)
 
         self.gridLayout.addWidget(self.SizeSpeed, 1, 0, 1, 1)
 
-        self.lineEidt_Size = QLineEdit(self.widget)
-        self.lineEidt_Size.setObjectName(u"lineEidt_Size")
+        self.lineEdit_Size = QDoubleSpinBox(self.widget)
+        self.lineEdit_Size.setObjectName(u"lineEdit_Size")
+        self.lineEdit_Size.setValue(1.000000000000000)
 
-        self.gridLayout.addWidget(self.lineEidt_Size, 1, 1, 1, 2)
+        self.gridLayout.addWidget(self.lineEdit_Size, 1, 1, 1, 2)
 
         self.label_10 = QLabel(self.widget)
         self.label_10.setObjectName(u"label_10")
@@ -75,13 +77,16 @@ class Ui_Controller(object):
 
         self.gridLayout.addWidget(self.label_2, 3, 1, 1, 1)
 
-        self.AngleSpeed = QLineEdit(self.widget)
+        self.AngleSpeed = QDoubleSpinBox(self.widget)
         self.AngleSpeed.setObjectName(u"AngleSpeed")
+        self.AngleSpeed.setValue(1.000000000000000)
 
         self.gridLayout.addWidget(self.AngleSpeed, 4, 0, 1, 1)
 
-        self.lineEdit_Angle = QLineEdit(self.widget)
+        self.lineEdit_Angle = QDoubleSpinBox(self.widget)
         self.lineEdit_Angle.setObjectName(u"lineEdit_Angle")
+        self.lineEdit_Angle.setMinimum(-1000.000000000000000)
+        self.lineEdit_Angle.setMaximum(1000.000000000000000)
 
         self.gridLayout.addWidget(self.lineEdit_Angle, 4, 1, 1, 2)
 
@@ -100,44 +105,74 @@ class Ui_Controller(object):
 
         self.gridLayout.addWidget(self.AngleUp, 5, 2, 1, 1)
 
+        self.label_15 = QLabel(self.widget)
+        self.label_15.setObjectName(u"label_15")
+
+        self.gridLayout.addWidget(self.label_15, 6, 0, 1, 1)
+
+        self.QCheckBoxMirror = QCheckBox(self.widget)
+        self.QCheckBoxMirror.setObjectName(u"QCheckBoxMirror")
+        self.QCheckBoxMirror.setChecked(False)
+
+        self.gridLayout.addWidget(self.QCheckBoxMirror, 6, 1, 1, 1)
+
         self.label_14 = QLabel(self.widget)
         self.label_14.setObjectName(u"label_14")
 
-        self.gridLayout.addWidget(self.label_14, 6, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_14, 7, 0, 1, 1)
 
-        self.OutlineEnable = QCheckBox(self.widget)
-        self.OutlineEnable.setObjectName(u"OutlineEnable")
+        self.QCheckBoxOutline = QCheckBox(self.widget)
+        self.QCheckBoxOutline.setObjectName(u"QCheckBoxOutline")
 
-        self.gridLayout.addWidget(self.OutlineEnable, 6, 1, 1, 1)
+        self.gridLayout.addWidget(self.QCheckBoxOutline, 7, 1, 1, 1)
+
+        self.label_16 = QLabel(self.widget)
+        self.label_16.setObjectName(u"label_16")
+
+        self.gridLayout.addWidget(self.label_16, 8, 0, 1, 1)
+
+        self.comboBoxColor = QComboBox(self.widget)
+        self.comboBoxColor.addItem("")
+        self.comboBoxColor.addItem("")
+        self.comboBoxColor.addItem("")
+        self.comboBoxColor.addItem("")
+        self.comboBoxColor.addItem("")
+        self.comboBoxColor.addItem("")
+        self.comboBoxColor.addItem("")
+        self.comboBoxColor.setObjectName(u"comboBoxColor")
+
+        self.gridLayout.addWidget(self.comboBoxColor, 8, 1, 1, 1)
 
         self.label_12 = QLabel(self.widget)
         self.label_12.setObjectName(u"label_12")
 
-        self.gridLayout.addWidget(self.label_12, 7, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_12, 9, 0, 1, 1)
 
-        self.horizontalSlider = QSlider(self.widget)
-        self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setValue(50)
-        self.horizontalSlider.setOrientation(Qt.Horizontal)
+        self.QSlider_threshold1 = QSlider(self.widget)
+        self.QSlider_threshold1.setObjectName(u"QSlider_threshold1")
+        self.QSlider_threshold1.setMaximum(1000)
+        self.QSlider_threshold1.setValue(50)
+        self.QSlider_threshold1.setOrientation(Qt.Horizontal)
 
-        self.gridLayout.addWidget(self.horizontalSlider, 7, 1, 1, 2)
+        self.gridLayout.addWidget(self.QSlider_threshold1, 9, 1, 1, 2)
 
         self.label_13 = QLabel(self.widget)
         self.label_13.setObjectName(u"label_13")
 
-        self.gridLayout.addWidget(self.label_13, 8, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_13, 10, 0, 1, 1)
 
-        self.horizontalSlider_2 = QSlider(self.widget)
-        self.horizontalSlider_2.setObjectName(u"horizontalSlider_2")
-        self.horizontalSlider_2.setValue(50)
-        self.horizontalSlider_2.setOrientation(Qt.Horizontal)
+        self.QSlider_threshold2 = QSlider(self.widget)
+        self.QSlider_threshold2.setObjectName(u"QSlider_threshold2")
+        self.QSlider_threshold2.setMaximum(1000)
+        self.QSlider_threshold2.setValue(50)
+        self.QSlider_threshold2.setOrientation(Qt.Horizontal)
 
-        self.gridLayout.addWidget(self.horizontalSlider_2, 8, 1, 1, 2)
+        self.gridLayout.addWidget(self.QSlider_threshold2, 10, 1, 1, 2)
 
         self.label_11 = QLabel(self.widget)
         self.label_11.setObjectName(u"label_11")
 
-        self.gridLayout.addWidget(self.label_11, 9, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_11, 11, 0, 1, 1)
 
         self.QSliderTransparency = QSlider(self.widget)
         self.QSliderTransparency.setObjectName(u"QSliderTransparency")
@@ -146,7 +181,7 @@ class Ui_Controller(object):
         self.QSliderTransparency.setValue(50)
         self.QSliderTransparency.setOrientation(Qt.Horizontal)
 
-        self.gridLayout.addWidget(self.QSliderTransparency, 9, 1, 1, 2)
+        self.gridLayout.addWidget(self.QSliderTransparency, 11, 1, 1, 2)
 
 
         self.retranslateUi(Controller)
@@ -158,22 +193,29 @@ class Ui_Controller(object):
         Controller.setWindowTitle(QCoreApplication.translate("Controller", u"Form", None))
         self.label_8.setText(QCoreApplication.translate("Controller", u"Size speed", None))
         self.label_9.setText(QCoreApplication.translate("Controller", u"Size", None))
-        self.SizeSpeed.setText(QCoreApplication.translate("Controller", u"0.01", None))
-        self.lineEidt_Size.setText(QCoreApplication.translate("Controller", u"1", None))
         self.label_10.setText(QCoreApplication.translate("Controller", u"Range 0-infity", None))
         self.SizeDown.setText(QCoreApplication.translate("Controller", u"-", None))
         self.SizeUp.setText(QCoreApplication.translate("Controller", u"+", None))
         self.label.setText(QCoreApplication.translate("Controller", u"Angle speed", None))
         self.label_2.setText(QCoreApplication.translate("Controller", u"Angle", None))
-        self.AngleSpeed.setText(QCoreApplication.translate("Controller", u"1.0", None))
-        self.lineEdit_Angle.setText(QCoreApplication.translate("Controller", u"0.0", None))
         self.label_5.setText(QCoreApplication.translate("Controller", u"Range 0-180", None))
         self.AngleDown.setText(QCoreApplication.translate("Controller", u"-", None))
         self.AngleUp.setText(QCoreApplication.translate("Controller", u"+", None))
+        self.label_15.setText(QCoreApplication.translate("Controller", u"Mirror", None))
+        self.QCheckBoxMirror.setText(QCoreApplication.translate("Controller", u"Enable", None))
         self.label_14.setText(QCoreApplication.translate("Controller", u"Outline", None))
-        self.OutlineEnable.setText(QCoreApplication.translate("Controller", u"Enable", None))
-        self.label_12.setText(QCoreApplication.translate("Controller", u"Contrast_x", None))
-        self.label_13.setText(QCoreApplication.translate("Controller", u"Contrast_y", None))
+        self.QCheckBoxOutline.setText(QCoreApplication.translate("Controller", u"Enable", None))
+        self.label_16.setText(QCoreApplication.translate("Controller", u"Outline Color", None))
+        self.comboBoxColor.setItemText(0, QCoreApplication.translate("Controller", u"White", None))
+        self.comboBoxColor.setItemText(1, QCoreApplication.translate("Controller", u"Blue", None))
+        self.comboBoxColor.setItemText(2, QCoreApplication.translate("Controller", u"Yellow", None))
+        self.comboBoxColor.setItemText(3, QCoreApplication.translate("Controller", u"Red", None))
+        self.comboBoxColor.setItemText(4, QCoreApplication.translate("Controller", u"Green", None))
+        self.comboBoxColor.setItemText(5, QCoreApplication.translate("Controller", u"Gold", None))
+        self.comboBoxColor.setItemText(6, QCoreApplication.translate("Controller", u"Black", None))
+
+        self.label_12.setText(QCoreApplication.translate("Controller", u"Threshold1", None))
+        self.label_13.setText(QCoreApplication.translate("Controller", u"Threshold2", None))
         self.label_11.setText(QCoreApplication.translate("Controller", u"Transparency", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -1,44 +1,101 @@
+import cv2  # 导入OpenCV库
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel, QVBoxLayout, QWidget
-from PySide6.QtGui import QPixmap, QTransform, QPainter
+from PySide6.QtGui import QPixmap, QTransform, QImage, QPainter
 from PySide6.QtCore import Qt, Signal, QSize, QPoint
 from transfer_shape_ui import Ui_TransferShape  # 引入生成的UI文件中的类
 from control_ui import Ui_Controller  # 引入生成的UI文件中的类
 import sys
 import math
+import numpy as np
 
 class Controller(QMainWindow):
     sizeChanged = Signal(float)  # Signal for size change
     angleChanged = Signal(float)  # Signal for angle change
     transparencyChanged = Signal(int)  # Signal for transparency change
+    mirrorChanged = Signal(bool)  # Signal for mirror change
+    outlineChanged = Signal(bool)  # Signal for outline change
+    threshold1Changed = Signal(int)  # Signal for threshold1 change
+    threshold2Changed = Signal(int)  # Signal for threshold2 change
+    colorChanged = Signal(str)  # Signal for color change
     
     def __init__(self):
         super(Controller, self).__init__()
         self.ui = Ui_Controller()
         self.ui.setupUi(self)
 
-        # Connect lineEdit_Size's textChanged signal to the slot function
-        self.ui.lineEidt_Size.textChanged.connect(self.on_size_changed)
-        # Connect lineEdit_Angle's textChanged signal to the slot function
-        self.ui.lineEdit_Angle.textChanged.connect(self.on_angle_changed)
-        # Connect QSliderTransparency's valueChanged signal to the slot function
+        # Connect UI signals to the slot functions
+        self.ui.lineEdit_Size.valueChanged.connect(self.on_size_changed)
+        self.ui.lineEdit_Angle.valueChanged.connect(self.on_angle_changed)
         self.ui.QSliderTransparency.valueChanged.connect(self.on_transparency_changed)
+        self.ui.QCheckBoxMirror.stateChanged.connect(self.on_mirror_changed)
+        self.ui.QCheckBoxOutline.stateChanged.connect(self.on_outline_changed)
+        self.ui.QSlider_threshold1.valueChanged.connect(self.on_threshold1_changed)
+        self.ui.QSlider_threshold2.valueChanged.connect(self.on_threshold2_changed)
+        self.ui.comboBoxColor.currentTextChanged.connect(self.on_color_changed)
 
-    def on_size_changed(self, text):
-        try:
-            scale = float(text)
-            self.sizeChanged.emit(scale)  # Emit sizeChanged signal
-        except ValueError:
-            pass  # Ignore invalid input
-        
-    def on_angle_changed(self, text):
-        try:
-            angle = float(text)
-            self.angleChanged.emit(angle)  # Emit angleChanged signal
-        except ValueError:
-            pass  # Ignore invalid input
+        # Connect buttons for size adjustment
+        self.ui.SizeDown.clicked.connect(self.on_size_down)
+        self.ui.SizeUp.clicked.connect(self.on_size_up)
+        # Connect buttons for angle adjustment
+        self.ui.AngleDown.clicked.connect(self.on_angle_down)
+        self.ui.AngleUp.clicked.connect(self.on_angle_up)
+
+    def on_size_changed(self, value):
+        self.sizeChanged.emit(value)  # Emit sizeChanged signal
+
+    def on_angle_changed(self, value):
+        self.angleChanged.emit(value)  # Emit angleChanged signal
 
     def on_transparency_changed(self, value):
         self.transparencyChanged.emit(value)  # Emit transparencyChanged signal
+    
+    def on_mirror_changed(self, state):
+        self.mirrorChanged.emit(state == 2)  # Emit mirrorChanged signal
+
+    def on_outline_changed(self, state):
+        self.outlineChanged.emit(state == 2)  # Emit outlineChanged signal
+
+    def on_threshold1_changed(self, value):
+        self.threshold1Changed.emit(value)  # Emit threshold1Changed signal
+
+    def on_threshold2_changed(self, value):
+        self.threshold2Changed.emit(value)  # Emit threshold2Changed signal
+
+    def on_color_changed(self, color):
+        self.colorChanged.emit(color)  # Emit colorChanged signal
+
+    def on_size_down(self):
+        try:
+            current_size = self.ui.lineEdit_Size.value()
+            new_size = max(0.0, current_size - self.ui.SizeSpeed.value())
+            self.ui.lineEdit_Size.setValue(new_size)
+        except ValueError:
+            pass  # Ignore invalid input
+
+    def on_size_up(self):
+        try:
+            current_size = self.ui.lineEdit_Size.value()
+            new_size = current_size + self.ui.SizeSpeed.value()
+            self.ui.lineEdit_Size.setValue(new_size)
+        except ValueError:
+            pass  # Ignore invalid input
+
+    def on_angle_down(self):
+        try:
+            current_angle = self.ui.lineEdit_Angle.value()
+            new_angle = current_angle - self.ui.AngleSpeed.value()
+            self.ui.lineEdit_Angle.setValue(new_angle)
+        except ValueError:
+            pass  # Ignore invalid input
+
+    def on_angle_up(self):
+        try:
+            current_angle = self.ui.lineEdit_Angle.value()
+            new_angle = current_angle + self.ui.AngleSpeed.value()
+            self.ui.lineEdit_Angle.setValue(new_angle)
+        except ValueError:
+            pass  # Ignore invalid input
+
 
 class ChildWindowMove(QMainWindow):
     moved = Signal(QPoint)  # 自定义信号，用于窗口移动时发送位置
@@ -46,11 +103,11 @@ class ChildWindowMove(QMainWindow):
 
     def __init__(self):
         super(ChildWindowMove, self).__init__()
-        self.setWindowTitle("Child Window")
+        self.setWindowTitle("Moving")
         self.setMouseTracking(True)
         self.resizing = False
 
-        self.label = QLabel("This is the child window", self)
+        self.label = QLabel("Please move this window, \n and the main window \n will move with it.", self)
         self.label.setAlignment(Qt.AlignCenter)
 
         layout = QVBoxLayout()
@@ -59,6 +116,8 @@ class ChildWindowMove(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+        
+        self.setMinimumSize(200, 100)  # Set minimum window size
 
     def moveEvent(self, event):
         super(ChildWindowMove, self).moveEvent(event)
@@ -89,7 +148,6 @@ class ChildWindowMove(QMainWindow):
         else:
             super(ChildWindowMove, self).mouseMoveEvent(event)
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -117,13 +175,28 @@ class MainWindow(QMainWindow):
         self.current_scale = 1.0  # Initial scale
         self.current_angle = 0.0  # Initial angle
         self.current_transparency = 255  # Initial transparency (0-255)
+        self.mirror_enabled = False  # Initial mirror state
+        self.outline_enabled = False  # Initial outline state
+        self.threshold1 = 100  # Initial threshold1 for edge detection
+        self.threshold2 = 200  # Initial threshold2 for edge detection
+        self.outline_color = "White"  # Initial color for outline
 
         self.setMinimumSize(200, 200)  # Set minimum window size
+        self.center_main_window()
+        self.create_child_window()
+        self.open_control_window()
 
+    def center_main_window(self):
+        screen_geometry = QApplication.primaryScreen().geometry()
+        x = (screen_geometry.width() - self.width()) // 2
+        y = (screen_geometry.height() - self.height()) // 2
+        self.move(x, y)
+    
     def open_image(self):
         file_name, _ = QFileDialog.getOpenFileName(self, "Open Image File", "", "Image Files (*.png *.jpg *.bmp)")
         if file_name:
             self.pixmap = QPixmap(file_name)  # Save original pixmap
+            self.image = cv2.imread(file_name)  # 使用OpenCV读取图像
             self.update_image_size()
 
     def update_image_size(self):
@@ -131,7 +204,15 @@ class MainWindow(QMainWindow):
             new_size = self.pixmap.size() * self.current_scale
             transformed_pixmap = self.pixmap.scaled(new_size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
             transform = QTransform().rotate(self.current_angle)
+            if self.mirror_enabled:
+                transform.scale(-1, 1)  # Apply mirror transformation
             transformed_pixmap = transformed_pixmap.transformed(transform, Qt.SmoothTransformation)
+
+            if self.outline_enabled:
+                # Convert image to contour image
+                contour_image = self.get_contour_image(self.image, self.threshold1, self.threshold2, self.outline_color)
+                qimage = QImage(contour_image.data, contour_image.shape[1], contour_image.shape[0], contour_image.strides[0], QImage.Format_ARGB32)
+                transformed_pixmap = QPixmap.fromImage(qimage)
 
             # Apply transparency
             transparent_pixmap = QPixmap(transformed_pixmap.size())
@@ -143,6 +224,32 @@ class MainWindow(QMainWindow):
 
             self.image_label.setPixmap(transparent_pixmap)
             self.resize_main_window_to_image(transformed_pixmap.size())
+
+    def get_contour_image(self, image, threshold1, threshold2, color_name):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, threshold1, threshold2)  # 使用Canny边缘检测
+
+        # 创建一个全透明的图像
+        transparent_image = np.zeros((edges.shape[0], edges.shape[1], 4), dtype=np.uint8)
+
+        # 颜色字典，颜色顺序为BGR
+        color_dict = {
+            "White": [255, 255, 255, 255],
+            "Blue": [255, 0, 0, 255],
+            "Yellow": [0, 255, 255, 255],
+            "Red": [0, 0, 255, 255],
+            "Green": [0, 255, 0, 255],
+            "Gold": [0, 215, 255, 255],
+            "Black": [0, 0, 0, 255]
+        }
+
+        color = color_dict.get(color_name, [255, 255, 255, 255])
+
+        # 设置轮廓部分为选定的颜色，其他部分为透明
+        transparent_image[edges != 0] = color
+        transparent_image[edges == 0] = [0, 0, 0, 0]  # 透明
+
+        return transparent_image
 
     def resize_main_window_to_image(self, size):
         diagonal_length = math.sqrt(size.width() ** 2 + size.height() ** 2)
@@ -171,6 +278,11 @@ class MainWindow(QMainWindow):
             self.control_window.sizeChanged.connect(self.on_scale_changed)  # Connect signal
             self.control_window.angleChanged.connect(self.on_angle_changed)  # Connect signal
             self.control_window.transparencyChanged.connect(self.on_transparency_changed)  # Connect signal
+            self.control_window.mirrorChanged.connect(self.on_mirror_changed)  # Connect signal
+            self.control_window.outlineChanged.connect(self.on_outline_changed)  # Connect signal
+            self.control_window.threshold1Changed.connect(self.on_threshold1_changed)  # Connect signal
+            self.control_window.threshold2Changed.connect(self.on_threshold2_changed)  # Connect signal
+            self.control_window.colorChanged.connect(self.on_color_changed)  # Connect signal
         
         # Move control window to bottom-right corner of the screen
         screen_geometry = QApplication.primaryScreen().geometry()
@@ -192,6 +304,29 @@ class MainWindow(QMainWindow):
     def on_transparency_changed(self, transparency):
         self.current_transparency = int((transparency / 100.0) * 255)
         self.update_image_size()
+
+    def on_mirror_changed(self, enabled):
+        self.mirror_enabled = enabled
+        self.update_image_size()
+
+    def on_outline_changed(self, enabled):
+        self.outline_enabled = enabled
+        self.update_image_size()
+
+    def on_threshold1_changed(self, value):
+        self.threshold1 = value
+        if self.outline_enabled:
+            self.update_image_size()
+
+    def on_threshold2_changed(self, value):
+        self.threshold2 = value
+        if self.outline_enabled:
+            self.update_image_size()
+
+    def on_color_changed(self, color):
+        self.outline_color = color
+        if self.outline_enabled:
+            self.update_image_size()
 
 
 if __name__ == "__main__":
